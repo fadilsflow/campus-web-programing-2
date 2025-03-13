9980df2c08584f78b5ba6b58e535b39b0f89ac42
fadilsflow <wahyufadil1140@gmail.com>
2025-03-13 16:35:49 +0700

    [Pertemuan 1] Installasi dan Pengenalan Laravel

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::get('/home', function () {
+    return '<h1>Hallow aku home</h1>
+            <p>this is a paragraph</p>
+        ';
+});
+
+Route::get('/profile', function () {
+    return "Hello, world!";
+});
+
+Route::get('/contact', function () {
+    return "Hello, world!";
+});
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+});
+
+require __DIR__.'/auth.php';
