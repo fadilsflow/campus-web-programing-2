00e08dfd83e2584575830a5af2827c9e71bb27ed
WAHYUAKHMADFADILLAH <wahyufadil1140@gmail.com>
2025-03-21 21:57:19 +0700

    [Pertemuan 3] Membuat Layout dan Component...

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,39 +1,46 @@
 <div>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-<div class="container-fluid">
-<a class="navbar-brand" href="/">E-Commerce</a>
-<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-aria-expanded="false" aria-label="Toggle navigation">
-<span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
-<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-<li class="nav-item">
-<a class="nav-link active" aria-current="page" href="/">Home</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="/products">Products</a>
-</li>
-<li class="nav-item dropdown">
-<a class="nav-link dropdown-toggle" href="/categories" role="button"
-data-bs-toggle="dropdown" aria-expanded="false">
-Categories
-</a>
-<ul class="dropdown-menu">
-<li><a class="dropdown-item" href="#">Pria</a></li>
-<li><a class="dropdown-item" href="#">Wanita</a></li>
-<li><hr class="dropdown-divider"></li>
-<li><a class="dropdown-item" href="#">Anak-Anak</a></li>
-</ul>
-</li>
-</ul>
-<form class="d-flex" role="search">
-<input class="form-control me-2" type="search" placeholder="Search"
-aria-label="Search">
-<button class="btn btn-outline-success" type="submit">Search</button>
-</form>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
+        <div class="container">
+            <a class="navbar-brand fw-bold" href="/">E-Commerce</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/products">Products</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Categories
+                        </a>
+                        <ul class="dropdown-menu bg-dark">
+                            <li><a class="dropdown-item text-light hover-effect" href="#">Pria</a></li>
+                            <li><a class="dropdown-item text-light hover-effect" href="#">Wanita</a></li>
+                            <li><hr class="dropdown-divider bg-light"></li>
+                            <li><a class="dropdown-item text-light hover-effect" href="#">Anak-Anak</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-light" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
 </div>
-</div>
-</nav>
-</div>
+
+<style>
+    .hover-effect:hover {
+        background-color: rgba(255, 255, 255, 0.2);
+    }
+</style>
