70673b6bbeabec84e29d6ec940858acdf2a51bf0
WAHYUAKHMADFADILLAH <wahyufadil1140@gmail.com>
2025-04-10 17:36:52 +0700

    vibe by windsurf 🥶

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -8,47 +8,11 @@ class HomepageController extends Controller
 {
     public function index()
     {
-        $categories = [
-            [
-                'id' => 1,
-                'name' => 'pria',
-                'slug' => 'Pakaian Pria',
-                'description' => 'Ini adalah produk pakaian pria',
-                'image' => 'https://placehold.co/300x300?text=Pakaian+Pria',
-            ],
-            [
-                'id' => 2,
-                'name' => 'wanita',
-                'slug' => 'Pakaian Wanita',
-                'description' => 'Ini adalah produk pakaian wanita',
-                'image' => 'https://placehold.co/300x300?text=Pakaian+Wanita',
-            ],
-            [
-                'id' => 3,
-                'name' => 'anak-anak',
-                'slug' => 'Pakaian Anak-Anak',
-                'description' => 'Ini adalah produk pakaian anak-anak',
-                'image' => 'https://placehold.co/300x300?text=Pakaian+Anak-Anak',
-            ],
-            [
-                'id' => 4,
-                'name' => 'aksesori',
-                'slug' => 'Aksesori',
-                'description' => 'Ini adalah produk aksesori',
-                'image' => 'https://placehold.co/300x300?text=Aksesori',
-            ],
-            [
-                'id' => 5,
-                'name' => 'sepatu',
-                'slug' => 'Sepatu',
-                'description' => 'Ini adalah produk sepatu',
-                'image' => 'https://placehold.co/300x300?text=Sepatu',
-            ]
-        ];
+       $categories = Categories::all();
 
-        return view('web.homepage', [
-            'categories' => $categories,
-        ]);
+ return view('web.homepage',[
+ 'categories' => $categories,
+ ]);
     }
 
     public function products()
